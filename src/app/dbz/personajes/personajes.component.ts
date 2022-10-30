d216de265bed2227ee104d2fc43dcc00767920c5
cesarluilly <cesarluilly.garcia@hotmail.com>
2022-10-30 02:09:57 -0500

    Video 61 Crear componentes hijos

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html'
+})
+export class PersonajesComponent {
+  public personajes:any[] = [];
+  
+}
